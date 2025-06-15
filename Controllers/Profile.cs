f0490f9730b7428bf74435b9439ff4a2d491f51c
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-06-16 03:31:35 +0800

    Add project files.

---
+++ Controllers/Profile.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace woolly_friends.Controllers
+{
+    public class Profile : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Admin
+        public IActionResult PetsPage() 
+        {
+            return View();
+        }
+
+        // Admin
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        // Admin
+        public IActionResult Reviews()
+        {
+            return View();
+        }
+    }
+}
