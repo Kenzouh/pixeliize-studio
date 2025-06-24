9af0ed69d43f8a86ab7d4025f9b419a83a87fc85
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-06-25 03:17:09 +0800

    feat: add adoption and payment forms
    fix: navbar interactivity and linkage

---
+++ Controllers/AdoptionForm.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace woolly_friends.Controllers
+{
+    public class AdoptionForm : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
