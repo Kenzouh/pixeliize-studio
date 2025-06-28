60c966e012a6144292a8568053e466d729c7c4fb
ED POGI 123 <georgeedwardditan@iskolarngbayan.pup.edu.ph>
2025-06-28 20:03:34 +0800

    feat: add session-based rendering in user profile page

--- Controllers/UserProfileController.cs
+++ Controllers/UserProfileController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using woolly_friends.Models;
 
 namespace woolly_friends.Controllers
 {
     public class UserProfileController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public UserProfileController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null) return RedirectToAction("Index", "Login");
+
+            var user = _context.Users.FirstOrDefault(u => u.Id == userId);
+            if (user == null) return RedirectToAction("Index", "Login");
+
+            return View(user);
         }
 
         public IActionResult Orders()
