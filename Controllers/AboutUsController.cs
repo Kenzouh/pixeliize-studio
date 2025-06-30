d19ee5bab0ad35d73e3cc015a1ed2c45d40b8658
jerichomrqz <marquezjericho48@gmail.com>
2025-06-30 11:08:19 +0800

    feat: add about us page

---
+++ Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace woolly_friends.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
