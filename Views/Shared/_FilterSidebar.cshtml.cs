c592ef3966e964870dd681ef11788d2a9167b188
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-07-02 21:13:26 +0800

    refactor: create components and enhance profile pages

---
+++ Views/Shared/_FilterSidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace woolly_friends.Views.Shared
+{
+    public class _FilterSidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
