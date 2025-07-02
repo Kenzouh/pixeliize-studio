5c25a7088b0d33a5594cb269add2514b411537db
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-07-02 08:16:39 +0800

    refactor: beautify PetsGallery, PetsPage and the sidebar filter

---
+++ Controllers/PetsGalleryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace woolly_friends.Controllers
+{
+    public class PetsGalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Pet()
+        {
+            return View();
+        }
+    }
+}
--- obj/Debug/net8.0/woolly-friends.AssemblyInfo.cs
+++ obj/Debug/net8.0/woolly-friends.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("woolly-friends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a4914b8ddf086f7030e30fdbdfe18bc886bc92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48d0b3e71aa814b9fb553919649039abbbb884c")]
 [assembly: System.Reflection.AssemblyProductAttribute("woolly-friends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("woolly-friends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
