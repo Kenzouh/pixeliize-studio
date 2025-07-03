83ed367699eabdd0b60bad96f75ba5d30e76de5b
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-07-04 00:37:39 +0800

    feat: add pet transaction rating form

---
+++ Controllers/PetsReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace woolly_friends.Controllers
+{
+    public class PetsReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
