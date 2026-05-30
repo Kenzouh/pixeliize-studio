cdb49c7581507894def9856a6cc01576194aacfb
Lawrenz Mikael Alcaparaz <lawrenzmikaelsalcaparaz@iskolarngbayan.pup.edu.ph>
2026-05-30 22:11:40 +0800

    fix: adjust modal z-index and resolve HTML page fetch.

--- obj/Debug/net8.0/woolly-friends.AssemblyInfo.cs
+++ obj/Debug/net8.0/woolly-friends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("woolly-friends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856da098c3dea1d49490549b74ed4ef6a6052ae0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1da151bcb31796b2463b650fcf3a53cf2c920ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("woolly-friends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("woolly-friends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
